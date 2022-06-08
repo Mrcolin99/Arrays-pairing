3cffe2b0aa97fb439121c09d9b7bdf523dba8705
Colin Ciervo <colinciervo@@gmail.com>
2022-06-08 16:30:50 -0400

    Added locations

--- friends.js
+++ friends.js
@@ -2,3 +2,6 @@ var arrays = ["Bill", "John", "Sally", "Ben", "Abbie"];
 console.log(arrays[2]);
 console.log(arrays[4]);
 console.log(arrays[0]);
+var location = ["Denver", "LA", "NY", "Austin", "PHX"];
+location.pop();
+console.log(location);
